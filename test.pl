05e5b8a18cc3e44089e3445b10bd17f6d48eb72f
marshki <marshall@morethantomorrow.org>
2021-03-17 18:02:52 -0400

    Perl, you are evil.

--- test.pl
+++ test.pl
@@ -1,35 +1,40 @@
 #!/usr/bin/perl
-use strict;
+use strict; 
 use warnings;
-
+ 
 our $date_today = `date`;
 print ($date_today = `date`);
 
 
-sub netid_prompt{
-  print "Enter you NetID: ";
+sub main{ 
 
-  chomp (my $netid = <STDIN>);
+  sub netid_prompt{
+    print "Enter you NetID: ";
 
-  return $netid;
-}
+    chomp (our $netid = <STDIN>);
+    return $netid;
+  }
+
+  netid_prompt();
+  print "Your netid is:$netid \n";
+} 
+
+&main();
+
+=pod 
 
 sub passwd_prompt{ 
 
   print "Enter your password: ";
 
   system ("stty -echo");
-  chomp (my $passwd = <STDIN>);
-  system ("stty echo");
+  chomp (our $passwd = <STDIN>);
+  system ("stty echo" );
 
   return $passwd;
 } 
 
-sub main{
-  netid_prompt();
-  passwd_prompt();
-}
 
 
 &main();
-
+=cut
