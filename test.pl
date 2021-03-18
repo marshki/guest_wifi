f98d387e70a7824a06c6f691661d717d80e25f16
marshki <marshall@morethantomorrow.org>
2021-03-18 11:43:30 -0400

    I C U, Perl!

--- test.pl
+++ test.pl
@@ -5,36 +5,33 @@ use warnings;
 our $date_today = `date`;
 print ($date_today = `date`);
 
+my $netid;
+my $passwd; 
 
-sub main{ 
 
-  sub netid_prompt{
-    print "Enter you NetID: ";
+sub netid_prompt {
+  print "Enter you NetID: ";
 
-    chomp (our $netid = <STDIN>);
-    return $netid;
-  }
+  chomp ($netid = <STDIN>);
+  return $netid;
+}
 
-  netid_prompt();
-  print "Your netid is:$netid \n";
-} 
-
-&main();
-
-=pod 
-
-sub passwd_prompt{ 
+sub passwd_prompt { 
 
   print "Enter your password: ";
 
   system ("stty -echo");
-  chomp (our $passwd = <STDIN>);
+  chomp ($passwd = <STDIN>);
   system ("stty echo" );
 
   return $passwd;
 } 
 
+sub main { 
 
+  netid_prompt();
+  passwd_prompt();
+
+} 
 
 &main();
-=cut
