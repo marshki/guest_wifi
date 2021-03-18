b5091ef4f3701dcfc94631522c8bc0eae89297c3
marshki <marshall@morethantomorrow.org>
2021-03-18 11:44:00 -0400

    update

--- bleh.py
+++ bleh.py
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 
 my $netid;
+my $passwd; 
 
 sub main {
 
@@ -13,8 +14,22 @@ sub main {
     return $netid;
   }
 
+
+  sub passwd_prompt { 
+
+    print "Enter your password: ";
+
+    system ("stty -echo");
+    chomp ($passwd = <STDIN>);
+    system ("stty echo" );
+
+    return $passwd;
+} 
+
+
   netid_prompt();
-  print "Your netid is: $netid \n";
+  passwd_prompt();
+  print "Your netid is: $netid and your password is: $passwd \n";
 }
 
 &main();
