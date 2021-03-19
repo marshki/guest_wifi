3f0b0049129d5261162295b658130f63df06c398
marshki <marshall@morethantomorrow.org>
2021-03-19 12:24:37 -0400

    update

--- curl_test.pl
+++ curl_test.pl
@@ -6,6 +6,6 @@ my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 my $netid = "";
 my $passy = "\";
 
-my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
+my $retrieve = `curl -u "${netid}:${passy}" "$url" -O`;
 
 print "$retrieve";
