c0212503fe9fa1fcde1be36f25ae399b81ae3cb5
marshki <marshall@morethantomorrow.org>
2021-03-19 09:57:43 -0400

    different approach towards passing creds, still no go

--- curl_test.pl
+++ curl_test.pl
@@ -3,11 +3,9 @@ use strict;
 use warnings;
 
 my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
-my $netid = ;
-my $passwd = ;
+my $netid = "";
+my $passwd = "";
 
-my $retrieve = `curl --user "{$netid}:{$passwd}" "$url"`;
+my $retrieve = `curl -X POST -F 'username="${netid}"' -F 'password="${passwd}"' "$url"`;
 
 print "$retrieve";
-
-
