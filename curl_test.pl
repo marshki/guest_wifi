ffc602c4f761817e717c0d874ffa2b2c44a2e745
marshki <marshall@morethantomorrow.org>
2021-03-19 13:09:44 -0400

    VICTORY! use single quotes to encapsulated the variable so as to deal with special chars.

--- curl_test.pl
+++ curl_test.pl
@@ -4,7 +4,7 @@ use warnings;
 
 my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 my $netid = "";
-my $passy = "\";
+my $passy = '';
 
 my $retrieve = `curl -u "${netid}:${passy}" "$url" -O`;
 
