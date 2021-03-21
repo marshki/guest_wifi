05b6e2aee3ff9df95759e3420f122937fbb98b2d
marshki <marshall@morethantomorrow.org>
2021-03-21 13:41:44 -0400

    not a test per say, but used for testing

---
+++ tests/curl_test.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict; 
+use warnings;
+
+my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
+my $netid = "";
+my $passy = '';
+
+my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
+
+print "$retrieve";
