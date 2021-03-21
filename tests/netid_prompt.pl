51a3031864f73388a4772440b643e5f645cc7dd5
marshki <marshall@morethantomorrow.org>
2021-03-21 13:35:20 -0400

    homebrew test

--- tests/netid_prompt.pl
+++ tests/netid_prompt.pl
@@ -1,8 +1,11 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict; 
 use warnings;
- 
+
 my $netid;
+my $test_0; 
+my $test_1;
+my $derp = "derp!";
 
 sub netid_prompt {
   print "Enter you NetID: ";
@@ -11,5 +14,29 @@ sub netid_prompt {
   return $netid;
 }
 
+sub test_0 {
+  $test_0 = $netid eq $netid; 
+  print "Comparing: $netid to: $netid \n";
+
+  if ($test_0 == 1) {  
+    print "True\n";
+  }
+  else {
+    print "False\n";
+ }
+}
+sub test_1 {
+  $test_1 = $netid eq $derp; 
+  print "Comparing: $netid to: $derp \n";
+
+  if ($test_1 == 1) {  
+    print "True\n";
+  }
+  else {
+    print "False\n";
+ }
+}
+
 netid_prompt();
-print "Your NetID is: $netid \n";
+test_0();
+test_1();
