67e0d90bc3851247d18db9cd1e940291bf03e7a4
marshki <marshall@morethantomorrow.org>
2021-03-23 19:27:41 -0400

    for testing

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -5,7 +5,13 @@ use warnings;
 my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 my $netid = "";
 my $passy = '';
+my $retrieve; 
+my $parser; 
 
+# my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
 my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
-
 print "$retrieve";
+
+
+my $parser = sed -n '/<td>/,/<\/td>/p' $parser;
+print "$parser";
