b7fcafb0c89766ef9418d8a00be57276155082b6
marshki <marshall@morethantomorrow.org>
2021-03-25 16:20:37 -0400

    still having a problem parsing variable after it is passed in awk/sed

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -6,16 +6,24 @@ my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 my $netid = "";
 my $passy = '';
 my $retrieve; 
-
+my $parser;
 
 # Pass creds to curl and extract ROI with "sed": 
-my $retrieve = `curl -u "${netid}:${passy}" "$url" |sed -n '/<td>/,/<\/td>/p'`;
+my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
 print "$retrieve";
 
+my $parser = (`awk '/<td>/{a=$0}END{print a}' "$retrieve"`);
+
+print "$parser";
+
+# This works: 
 # Pass creds to curl and get page: 
 # my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
 # print "$retrieve";
 
+#my $parser = `sed -n '/<td>/,/\</td>/p' "$retrieve"`;
+
+# This works:
 # Pass creds to curl and extract ROI with "grep":
 # my $retrieve = `curl -u "${netid}:${passy}" "$url" |grep table| grep Guest| grep Password`;
 # print "$retrieve";
