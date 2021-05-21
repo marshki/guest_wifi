6777f62f51918936fa254b186db586920b6b6e5e
marshki <marshall@morethantomorrow.org>
2021-05-21 13:17:40 -0400

    add

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -2,30 +2,25 @@
 use strict; 
 use warnings;
 
-my $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
-my $netid = "mjk235";
-my $passy = "";
-my $retrieve; 
-my $parser;
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
+
+my $netid = "";
+my $passy = "\";
 
 # Pass creds to curl and extract ROI via regex parse: 
-my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
-print "$retrieve";
 
-#my $parser = (`grep table|grep Guest|grep Password "$retrieve"`); 
-#print "$parser";
+# Grep works, but can be more precise: 
 
-# This works: 
-# Pass creds to curl and get page: 
-# my $retrieve = `curl -u "${netid}:${passy}" "$url"`;
-# print "$retrieve";
+my $retrieve = `curl --user "${netid}:${passy}" "$url" > output.txt
 
-# This works, too:
-# Pass creds to curl and extract ROI with "grep":
-#my $retrieve = `curl -u "${netid}:${passy}" "$url" |grep table| grep Guest| grep Password`;
-#print "$retrieve";
+# |grep table |grep Guest |grep Password`;
 
+# Awk works: 
 # These work on a file, but not, for some reason, when using a pipe: 
+
+# my $retrieve = `curl --user "${netid}:${passy}" "$url" |awk '\<td>\{a=$0}END{print a}'`;
+
+print "$retrieve";
 
 # my $parser = `sed -n '/<td>/,/\</td>/p' "$retrieve"`;
 # my $parser = (`awk '\<td>\{a=$0}END{print a}' "$retrieve"`);
