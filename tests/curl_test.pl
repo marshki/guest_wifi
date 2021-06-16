eaee9d7b145822277aea1223d07477ffe796838d
marshki <marshall@morethantomorrow.org>
2021-06-16 19:21:23 -0400

    more poopsicles

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -4,29 +4,30 @@ use warnings;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
 
-my $netid = "\";
-my $passy = "\";
+my $netid = "" ;
+my $passy = "\" ;
 
 # Pass credentials to curl, then retireve ROI.
 # Grep table: 
 
-# my $retrieve_table = `curl --user "${netid}:${passy}" "$url" |grep table |grep Guest| grep Password` ;
-# print "$retrieve_table" ;
-
-my $parse_username = `curl --user "${netid}:${passy}" "$url" 2>/dev/null | awk -F '</*pre>' '$2{print $2}'` ;
-
-print "$parse_username" ; 
-
+#my $retrieve_table = `curl --user "${netid}:${passy}" "$url" |grep table |grep Guest| grep Password` ;
 #print "$retrieve_table" ;
 
-# my $parse_username = `awk -F '</*pre>' '$2{print $2}' "$retrieve_table"` ; 
+my $retrieve_table = `curl --user "${netid}:${passy}" "$url"` ;
+print "$retrieve_table" ;
+
+my $parse_username = `$retrieve_table awk -F '</*pre>' '$2{print $2}'` ;
+
+#print "$parse_username" ; 
+
+
+#my $parse_username = `awk -F '</*pre>' '$2{print $2}' "$retrieve_table"` ; 
 
 #print "$parse_username" ; 
 
 # awk -F '</*pre>' '$2{print $2}' parseMe
 
 # awk -F '</*td>' '$2{print $7}' index.pl
-
 
 ################################################################################
 # Pass credentials to curl, then retireve ROI.
