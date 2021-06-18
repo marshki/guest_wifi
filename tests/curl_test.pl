cf16c9ef32cbbfd7b0b5c86755f1e9e8139856f2
marshki <marshall@morethantomorrow.org>
2021-06-18 18:02:44 -0400

    turn on diagnostics

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -1,25 +1,27 @@
 #!/usr/bin/perl
 use strict; 
 use warnings;
+use diagnostics;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
 
-my $netid = "" ;
+my $netid = "\" ;
 my $passy = "\" ;
 
 # Pass credentials to curl, then retireve ROI.
 # Grep table: 
 
-#my $retrieve_table = `curl --user "${netid}:${passy}" "$url" |grep table |grep Guest| grep Password` ;
-#print "$retrieve_table" ;
-
-my $retrieve_table = `curl --user "${netid}:${passy}" "$url"` ;
+my $retrieve_table = `curl --user "${netid}:${passy}" "$url" |grep table |grep Guest| grep Password` ;
 print "$retrieve_table" ;
 
-my $parse_username = `$retrieve_table awk -F '</*pre>' '$2{print $2}'` ;
+#my $retrieve_table = `curl --user "${netid}:${passy}" "$url"` ;
+#print "$retrieve_table" ;
 
-#print "$parse_username" ; 
+my $awk_parser = `awk -F '</*pre>' '$2{print $2}' `; #"$retrieve_table"` ;
 
+my $parse_username = `"$awk_parser" "retrieve_table"` ; 
+
+print "$parse_username" ; 
 
 #my $parse_username = `awk -F '</*pre>' '$2{print $2}' "$retrieve_table"` ; 
 
