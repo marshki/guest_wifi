61d5d5f6dd5cdbd89868d50683aa91f9d6a0f630
marshki <marshall@morethantomorrow.org>
2021-06-21 13:33:43 -0400

    rm stale comments

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -8,24 +8,20 @@ our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
 my $netid = "" ;
 my $passy = "\" ;
 
-# Pass credentials to curl, extracting HTML. 
+# Pass credentials to curl, assigning HTML to variable.  
 
 my @retrieve_HTML = (`curl --user "${netid}:${passy}" "$url"`) ;
 
-# Extract region of interest (ROI).
+# Extract region of interest (ROI) from HTML.
 
 my $parse_HTML = (grep { /table.*Guest.*Password/ } "@retrieve_HTML")[0] ;
 
-# Extract username. 
+# Extract username from ROI.
 
 my $guest_username = (split qr{</?td>}, "$parse_HTML")[6];
 print "$guest_username" ; 
 
-# Extract password.
+# Extract password from ROI.
 
 my $guest_password = (split qr{</?pre>}, "$parse_HTML")[1];
 print "$guest_password" ; 
-
-# awk -F '</*pre>' '$2{print $2}' parseMe
-
-# awk -F '</*td>' '$2{print $7}' parseMe
