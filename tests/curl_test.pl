d71efbf021210444235d2ebbc5afaec525175b0d
marshki <marshall@morethantomorrow.org>
2021-06-25 12:54:26 -0400

    update print statement

--- tests/curl_test.pl
+++ tests/curl_test.pl
@@ -7,7 +7,7 @@ use diagnostics;
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
 
 my $netid = "" ;
-my $passy = "\" ;
+my $passy = "" ;
 
 # Pass credentials to curl, assigning HTML to variable.  
 
@@ -20,9 +20,9 @@ my $parse_HTML = (grep { /table.*Guest.*Password/ } @retrieve_HTML)[0] ;
 # Extract username from ROI.
 
 my $guest_username = (split qr{</?td>}, $parse_HTML)[6];
-print "$guest_username \n" ; 
+print "Guest username: $guest_username \n" ; 
 
 # Extract password from ROI.
 
 my $guest_password = (split qr{</?pre>}, $parse_HTML)[1];
-print "$guest_password \n" ; 
+print "Guest password: $guest_password \n" ; 
