86b030313ce8b69a8b19b5ed196e0210f3af72c6
marshki <marshall@morethantomorrow.org>
2021-06-27 11:42:47 -0400

    add; needs exception handling

---
+++ tests/retrieve_HTML.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
+
+my $netid = "" ;
+my $password = "" ;
+my $HTML;
+
+=begin retrieve_HTML
+Pass credentials to curl, assigining extracted page to $HTML.
+=cut
+
+sub retrieve_HTML {
+  print "Retrieving HTML from NYUROAM page... \n";
+
+  my @HTML = (`curl --user ${netid}:${password} $url`) ;
+
+  print "@HTML"; 
+}
+
+retrieve_HTML();
