01a9089ffed654c4d6bd1b10cf3c38f59c65977a
marshki <marshall@morethantomorrow.org>
2021-06-28 13:49:24 -0400

    not yet ready for prime time

--- tests/parse_HTML.pl
+++ tests/parse_HTML.pl
@@ -3,26 +3,35 @@ use strict;
 use warnings;
 use diagnostics;
 
-my $HTML='
-       <table>
-        <td><table border="1" cellspacing="0" cellpadding="5"><tr bgcolor="#AAAAAA" align="center" valign="center"><td><b>Guest ID</b></td> <td><b>Password</b></td></tr><tr bgcolor="#FFFFFF" align="center" valign="center"><td>guest160</td> <td><pre>plangogu</pre></td></tr></table>
-                 </td>
-                 </tr>
-                 </table>
-';
- 
-my $HTML_table;
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
+
+my $netid = "" ;
+my $password = "" ;
 
 =begin parse_HTML
 Extract region of interest (table) from HTML.
 =cut
 
+sub retrieve_HTML {
+  print "Retrieving HTML from NYUROAM page... \n";
+
+  our $HTML = (`curl --user ${netid}:${password} $url`) ;
+ 
+}
+
 sub parse_table {
   print "Parsing HTML from NYUROAM page... \n";
 
-  @HTML_table = (grep { /table.*Guest.*Password/ } @HTML)[0];
+  our $HTML_table = (grep { /table.*Guest.*Password/ } $HTML)[0];
 
-  print "@HTML_table"; 
+  print "$HTML_table"; 
 }
 
-parse_table();
+
+sub main() {
+
+  retrieve_HTML();
+  parse_table();
+}
+
+&main();
