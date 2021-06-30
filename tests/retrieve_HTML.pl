d518973a516133b16337681306dac8e973046235
marshki <marshall@morethantomorrow.org>
2021-06-30 18:32:53 -0400

    rm space before semicolons

--- tests/retrieve_HTML.pl
+++ tests/retrieve_HTML.pl
@@ -3,19 +3,19 @@ use strict;
 use warnings;
 use diagnostics;
 
-our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl" ;
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
-my $netid = "" ;
-my $password = "" ;
+my $netid = "";
+my $password = "";
 
 =begin retrieve_HTML
 Pass credentials to curl, assigining extracted page to $HTML.
 =cut
 
 sub retrieve_HTML {
-  print "Retrieving HTML from NYUROAM page... \n" ;
+  print "Retrieving HTML from NYUROAM page... \n";
 
-  my $HTML = (`curl --user ${netid}:${password} $url`) ;
+  my $HTML = (`curl --user ${netid}:${password} $url`);
 
 }
 
