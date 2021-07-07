1d6409fbf0bc84d66b9a1ba9a4933054b4f8bad8
marshki <marshall@morethantomorrow.org>
2021-07-07 13:34:56 -0400

    add starter code

--- tests/URL_status_check.pl
+++ tests/URL_status_check.pl
@@ -1,3 +1,17 @@
-Something like this would be helpful:
+#!/usr/bin/perl
 
-https://github.com/marshki/matlab_installer/blob/master/functions/curl_url_check.sh
+use strict;
+use warnings;
+use diagnostics;
+
+#Something like this would be helpful:
+#https://github.com/marshki/matlab_installer/blob/master/functions/curl_url_check.sh
+
+sub URL_status_check {
+  if(expression) {
+    print "Up!\n";
+} else {
+  print "Down!\n";
+}
+
+URL_status_check();
