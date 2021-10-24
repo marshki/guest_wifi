b3b5f483001ff74ecfbec67d2a71722eb1d5bd5b
marshki <marshall@morethantomorrow.org>
2021-10-24 18:24:58 -0400

    add

--- lib/guest_wifi.pl
+++ lib/guest_wifi.pl
@@ -7,13 +7,25 @@ use diagnostics;
 =pod
 =head1 DESCRIPTION
 
-Extract weekly "nyuguest" WiFi credentials in Perl.
+Extract weekly "nyuguest" WiFi credentials. 
 
 =head2 NAME
 
 get_guest_wifi.pl
 
 =head2 SYNOPSIS
+
+Extract weekly "nyuguest" WiFi credentials using Perl. 
+Requires access to NYU-NET--campus wide-area network (WAN)--and a valid NetID.
+Credentials are valid for one(1) week from 12:00:01 EST Monday.
+
+Sample output: 
+
+| Guest Username| Guest Password |
+| -----------   | -----------    |
+| guest160      | ataichme       |
+
+#### AUTHOR
 
 =head2 AUTHOR
 
@@ -22,6 +34,7 @@ get_guest_wifi.pl
 =head2 LICENSE
 
 Copyright <2021> <M. Krinitz>
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
