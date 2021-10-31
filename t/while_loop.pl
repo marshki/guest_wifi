98fe6527948ec9d82b29f21611477f68d40568ca
marshki <marshall@morethantomorrow.org>
2021-10-31 12:07:24 -0400

    correct while loop

--- t/while_loop.pl
+++ t/while_loop.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl -w
-# https://stackoverflow.com/questions/21665179/terminating-while-loop-with-empty-stdin-in-perl
 
 use strict;
 use warnings;
@@ -12,14 +11,16 @@ Prompt user for NetID.
 
 sub netid_prompt {
 
-  local while (1) {
+  while (1) {
     print "Enter your NetID: ";
 
     chomp($netid = <STDIN>);
-    return $netid;
-
+              
     last if $netid ne '';
-    }
-}
+    print "Try again!\n";
+    
+  }
+  return $netid;
+}    
 
 netid_prompt;
