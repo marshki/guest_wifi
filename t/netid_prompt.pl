d0feb235dd99fab56b6243ab91cdd9091d719116
marshki <marshall@morethantomorrow.org>
2021-10-31 12:46:16 -0400

    add

---
+++ t/netid_prompt.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+
+use Test::Simple tests => 1;
+
+my $netid;
+
+=begin netid_prompt
+Prompt user for input. Null string is not valid.
+Returns: netid
+=cut
+
+sub netid_prompt {
+
+  while (1) {
+    print "Enter your NetID: ";
+
+    chomp($netid = <STDIN>);
+              
+    last if $netid ne '';
+    print "No input detected!\n";
+    
+  }
+  return $netid;
+}
+
+netid_prompt;
+
+ok( $netid eq $netid );
