d2c78a7d71f592d4e4d6ac1db0cd65ca08cc99e2
marshki <marshall@morethantomorrow.org>
2021-11-26 13:52:42 -0500

    update; testing

--- t/test_auth.pl
+++ t/test_auth.pl
@@ -1,16 +1,25 @@
 #!/usr/bin/perl -w
 
-#use strict;
+use v5.30;
+
+use strict;
 use warnings;
-use v5.10;
+use diagnostics;
+
 use LWP::UserAgent;
 use HTTP::Request::Common;
-use Mozilla::CA module;
+use Mozilla::CA;
+
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
      
 my $ua = LWP::UserAgent->new();
-my $request = GET 'https://nyuroam-guest.nyu.edu/cgi-bin/index.pl';
+my $request = GET $url;
+
+my $netid = "";
+my $password = "\";
+# my @HTML;
      
-$request->authorization_basic('netID', 'password');
+$request->authorization_basic($netid, $password);
      
 my $response = $ua->request($request);
 say $response->as_string();
