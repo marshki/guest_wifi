03bc644a35a81910b45483c433f083ab138f41bd
marshki <marshall@morethantomorrow.org>
2021-11-28 14:23:29 -0500

    add sub routine

--- t/test_auth.pl
+++ t/test_auth.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl -w
-# For reference: https://metacpan.org/pod/LWP::UserAgent
+
+# For reference:
+# https://metacpan.org/pod/LWP::UserAgent
 
 use v5.30;
 
@@ -8,17 +10,15 @@ use warnings;
 use diagnostics;
 
 use LWP::UserAgent;
-use HTTP::Request::Common;
-use Mozilla::CA;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
+
+my $netid = "";
+my $password = "";
      
 my $ua = LWP::UserAgent->new();
-my $request = GET $url;
 
-my $netid = "\";
-my $password = "\";
-my @HTML;
+my $req = new HTTP::Request(GET => $url);
 
 =begin scrape_HTML
 Scrape, then return parsed table to "HTML".
@@ -27,11 +27,10 @@ Scrape, then return parsed table to "HTML".
 sub scrape_HTML {
   print "Scraping HTML from NYUROAM page... \n";
 
-  return @HTML = ($request->authorization_basic($netid, $password));
-
+  return $req->authorization_basic($netid, $password);
 }
    
 scrape_HTML();
   
-#my $response = $ua->request($request);
-#say $response->as_string();
+my $response = $ua->request($req);
+say $response->as_string();
