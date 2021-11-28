cff03a4357b0ad34077e7b47368275cba516b54e
marshki <marshall@morethantomorrow.org>
2021-11-28 12:48:45 -0500

    update, works but needs a subroutine

--- t/derp.pl
+++ t/derp.pl
@@ -8,9 +8,14 @@ use diagnostics;
 
 use LWP::UserAgent;
 
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
+
+my $netid = "\";
+my $password = "\";
+
 my $ua = LWP::UserAgent->new();
-my $req = new HTTP::Request(GET => 'https://nyuroam-guest.nyu.edu/cgi-bin/index.pl');
-$req->authorization_basic('netID','passwd');
+my $req = new HTTP::Request(GET => $url);
+$req->authorization_basic($netid, $password);
 my $response = $ua->request($req);
 
 say $response->as_string();
