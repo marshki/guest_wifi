98a0c66b778d2019d06edfa11dc063b7e3827be0
marshki <marshall@morethantomorrow.org>
2021-12-02 11:32:44 -0500

    replace say with print

--- t/scrape_HTML.pl
+++ t/scrape_HTML.pl
@@ -1,16 +1,24 @@
 #!/usr/bin/perl -w
 
+use v5.30;
+
 use strict;
 use warnings;
 use diagnostics;
 
-use Test::Simple tests => 1;
+use LWP::UserAgent;
+use Mozilla::CA;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
 my $netid = "";
-my $password = "\";
-my @HTML; 
+my $password = "";
+     
+my $ua = LWP::UserAgent->new();
+
+my $req = new HTTP::Request(GET => $url);
+
+my $response;
 
 =begin scrape_HTML
 Scrape, then return parsed table to "HTML".
@@ -19,10 +27,10 @@ Scrape, then return parsed table to "HTML".
 sub scrape_HTML {
   print "Scraping HTML from NYUROAM page... \n";
 
-  return @HTML = (`curl --user ${netid}:${password} $url`);
-
+  $req->authorization_basic($netid, $password);
+  return $response = $ua->request($req);
 }
 
 scrape_HTML();
-
-ok( @HTML eq @HTML );
+  
+print $response->as_string();
