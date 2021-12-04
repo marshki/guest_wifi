99009a8c630d02386745bcac095a767d2270066d
marshki <marshall@morethantomorrow.org>
2021-12-04 13:12:48 -0500

    add test; need to add auth

---
+++ t/v.1/test_URL.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+
+require LWP::UserAgent;
+ 
+my $ua = LWP::UserAgent->new;
+$ua->timeout(10);
+ 
+my $response = $ua->get('https://nyuroam-guest.nyu.edu/cgi-bin/index.pl/');
+ 
+if ($response->is_success) {
+  print $response->content;  # or whatever
+}
+else {
+  die $response->status_line;
+};
