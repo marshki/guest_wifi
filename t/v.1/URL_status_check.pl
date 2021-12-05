3eb4715012db4076d95afd72cb86f4fc53c6833d
marshki <marshall@morethantomorrow.org>
2021-12-05 12:33:31 -0500

    add code for test using LWP::Agent

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -1,28 +1,51 @@
 #!/usr/bin/perl -w
 
+# Response codes:
+# --------------
 # Off LAN: 599
 # On LAN, avec credentials:
 # On LAN, sans credentials: 401
+
+use v5.30;
 
 use strict;
 use warnings;
 use diagnostics;
 
+use LWP::UserAgent;
+use Mozilla::CA;
 
-my @urls = (
-    'https://www.google.com',
-    'http://nosuchsiteexists.com',
-    'https://nyuroam-guest.nyu.edu/cgi-bin/index.pl',
-);
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
-for my $url (@urls) {
-    my $response = $Client->get($url);
-    say $url, ": ", $response->{status};
+my $netid = "";
+my $password = "";
+     
+my $ua = LWP::UserAgent->new();
+$ua->timeout(10);
+
+my $req = new HTTP::Request(GET => $url);
+
+$req->authorization_basic($netid, $password);
+my $response = $ua->request($req);
+
+if ($response->is_success) {
+  print $response->content; 
 }
+else {
+  die $response->status_line;
+};
 
-# TODO: 
-# * pass creds to POST request
-# * get good response code
-# * if then for 200 status
+
+#my @urls = (
+#    'https://www.google.com',
+#    'http://nosuchsiteexists.com',
+#    'https://nyuroam-guest.nyu.edu/cgi-bin/index.pl',
+#);
+
+#for my $url (@urls) {
+#    my $response = $Client->get($url);
+#    say $url, ": ", $response->{status};
+#}
 
 #URL_status_check();
+
