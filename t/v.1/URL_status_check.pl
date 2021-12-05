d752ffc613281cd2cae0cee85814373d07d91772
marshki <marshall@morethantomorrow.org>
2021-12-05 12:27:57 -0500

    trying a different approach

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -1,20 +1,13 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 # Off LAN: 599
 # On LAN, avec credentials:
 # On LAN, sans credentials: 401
 
-# Tiny doc: https://metacpan.org/pod/HTTP%3a%3aTiny
-
 use strict;
 use warnings;
 use diagnostics;
 
-use feature 'say';
-
-use HTTP::Tiny;
-
-my $Client = HTTP::Tiny->new();
 
 my @urls = (
     'https://www.google.com',
@@ -27,17 +20,9 @@ for my $url (@urls) {
     say $url, ": ", $response->{status};
 }
 
-
 # TODO: 
 # * pass creds to POST request
 # * get good response code
 # * if then for 200 status
-
-#sub URL_status_check {
-#  if(expression) {
-#    print "In reach!\n";
-#} else {
-#  print "Out of reach!\n";
-#}
 
 #URL_status_check();
