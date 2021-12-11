9e64032f9e5ac9615f33f046a2b12c5e3ad7c2ba
marshki <marshall@morethantomorrow.org>
2021-12-11 12:58:44 -0500

    add note

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -13,6 +13,10 @@ our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
 my $netid = "";
 my $password = "";
+
+=begin URL_status_check
+Check HTTP response code.
+=cut
      
 my $ua = LWP::UserAgent->new();
 $ua->timeout(10);
@@ -22,13 +26,18 @@ my $req = new HTTP::Request(GET => $url);
 $req->authorization_basic($netid, $password);
 my $response = $ua->request($req);
 
+
 if ($response->is_success) {
   #print "I see you fam.\n";
-  #print $response->content; 
-  print $response->status_line;
+  print $response->content; 
+  #print $response->status_line;
 }
 else {
   die $response->status_line;
 };
 
-# URL_status_check();
+# This test works on the LAN, but not off it. For example:
+# Uncaught exception from user code:
+# 500 Can't connect to nyuroam-guest.nyu.edu:443 (Connection timed out) at URL_status_check.pl 
+
+#URL_status_check();
