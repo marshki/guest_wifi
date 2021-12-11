b817d7dd2db00af439fd52041f149c4f801e407d
marshki <marshall@morethantomorrow.org>
2021-12-11 13:10:13 -0500

    add

---
+++ t/v.1/silent_prompt.pl
@@ -0,0 +1,24 @@
+sub silent_prompt {
+    require Term::ReadKey;
+
+    # Do not show typed chars
+    Term::ReadKey::ReadMode('noecho');
+
+    print "Enter your password: ";
+    my $password = Term::ReadKey::ReadLine(0);
+
+    # Reset the terminal
+    Term::ReadKey::ReadMode('restore');
+
+    # This should print zilch!
+    print "\n";
+
+    # get rid of line ending (works on Windows, too!)
+    $password =~ s/\R\z//;
+
+    # say "Password was <$password>"; # check what you are doing :)
+
+    return $password;
+}
+
+silent_prompt;
