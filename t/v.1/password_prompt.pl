c9aad2650690660a19da64afd9127aa135525e38
marshki <marshall@morethantomorrow.org>
2021-12-24 12:28:49 -0500

    add password prompt (purely Perl)

---
+++ t/v.1/password_prompt.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl -w 
+
+use v5.30;
+
+use strict;
+use warnings; 
+use diagnostics;
+
+use Term::ReadKey;
+use Test::Simple tests => 1;
+
+my $password;
+
+=begin password_prompt 
+Prompt user for input. Null string is not valid.
+Returns: string
+=cut
+
+sub password_prompt {
+
+while (1) {
+  print "Enter your password: \n";
+
+  Term::ReadKey::ReadMode('noecho');
+  chomp ($password = Term::ReadKey::ReadLine(0));
+  Term::ReadKey::ReadMode('restore');
+  
+  last if $password ne '';
+  print "No input detected!\n";
+
+  }
+  return $password;
+}
+
+password_prompt;
+
+ok( $password eq $password );
