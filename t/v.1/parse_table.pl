a067d671853a716d28a8b304820aa15066451942
marshki <marshall@morethantomorrow.org>
2021-12-24 13:17:53 -0500

    refactor scrape; parse pending

--- t/v.1/parse_table.pl
+++ t/v.1/parse_table.pl
@@ -1,18 +1,26 @@
 #!/usr/bin/perl -w
+use v5.30;
 
 use strict;
 use warnings;
 use diagnostics;
 
-use Test::Simple tests => 2;
+#use Test::Simple tests => 1;
+
+use LWP::UserAgent;
+use Mozilla::CA;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
 my $netid = "";
 my $password = "";
 my @HTML;
+     
+my $ua = LWP::UserAgent->new();
 
-our $parse_HTML;
+my $req = new HTTP::Request(GET => $url);
+
+our $HTML;
 
 =begin scrape_HTML
 Scrape, then return parsed table to "HTML".
@@ -20,27 +28,30 @@ Scrape, then return parsed table to "HTML".
 
 sub scrape_HTML {
   print "Scraping HTML from NYUROAM page... \n";
-  return @HTML = (`curl --user ${netid}:${password} $url`);
+
+  $req->authorization_basic($netid, $password);
+  return $HTML = $ua->request($req);
 }
 
-=begin parse_table
-Extract region of interest (ROI) from HTML.
-=cut
+scrape_HTML();
+print $HTML->as_string();
+
+# ok( @HTML eq @HTML );
 
 sub parse_table {
   print "Parsing region of interest (ROI) from HTML... \n";
 
-  return $parse_HTML = (grep { /table.*Guest.*Password/ } @HTML) [0];
+  # return $parse_HTML = (grep { /table.*Guest.*Password/ } @HTML) [0];
 
 }
 
 sub main() {
   scrape_HTML();
-  parse_table();
-  print $parse_HTML;
+  #parse_table();
+  #print $parse_HTML;
 }
 
 &main();
 
-ok( @HTML eq @HTML );
-ok( $parse_HTML eq $parse_HTML );
+#ok( @HTML eq @HTML );
+#ok( $parse_HTML eq $parse_HTML );
