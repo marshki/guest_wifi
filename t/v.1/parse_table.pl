318316b359ed7142436661a72660dee2c8e0cbbd
marshki <marshall@morethantomorrow.org>
2021-12-26 13:05:39 -0500

    add, testing

--- t/v.1/parse_table.pl
+++ t/v.1/parse_table.pl
@@ -6,7 +6,6 @@ use warnings;
 use diagnostics;
 
 #use Test::Simple tests => 1;
-
 use LWP::UserAgent;
 use Mozilla::CA;
 
@@ -14,15 +13,14 @@ our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
 my $netid = "";
 my $password = "";
+
 my @HTML;
+our $parse_HTML;
      
 my $ua = LWP::UserAgent->new();
-
 my $req = new HTTP::Request(GET => $url);
 
 our $HTML;
-
-our $first;
 
 =begin scrape_HTML
 Scrape, then return parsed table to "HTML".
@@ -43,15 +41,14 @@ scrape_HTML();
 sub parse_table {
   print "Parsing region of interest (ROI) from HTML... \n";
 
-  # return $parse_HTML = (grep { /table.*Guest.*Password/ } @HTML) [0];
-  # Perl wants you to use an HTML parser, not Perl, to extract stuff.
-  return $first = (split qr{</?td>}, @HTML) [6];
+  # Perl wants you to use an HTML parser, not Perl, but here's an attempt: 
+  return $parse_HTML =~ s/^<td>// @HTML;
 }
 
 sub main() {
   scrape_HTML();
   parse_table();
-  print $first;
+  print $parse_HTML;
 }
 
 &main();
