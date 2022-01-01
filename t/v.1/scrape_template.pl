a4ef9ff0c55d67198fece3ef868504154ec33241
marshki <marshall@morethantomorrow.org>
2022-01-01 15:31:43 -0500

    add content to return statement

--- t/v.1/scrape_template.pl
+++ t/v.1/scrape_template.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl -w 
 
-# Template for web scraping using LWP::UserAgent
-# For reference: 
-# https://metacpan.org/pod/LWP::UserAgent
-# Needed in some environments for SSL certs:
-# https://metacpan.org/pod/Mozilla::CA
+=begin comments
+Template for web scraping using LWP::UserAgent
+For reference: https://metacpan.org/pod/LWP::UserAgent
+Some environments require Mozilla's CA cert bundle 
+For reference:
+https://metacpan.org/pod/Mozilla::CA
+=cut
 
 use v5.30;
 
@@ -15,14 +17,9 @@ use diagnostics;
 use LWP::UserAgent;
 use Mozilla::CA; 
 
-our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
-
-my $netid = "";
-my $password = "";
+our $url = "";
 
 my $ua = LWP::UserAgent->new();
 my $req = new HTTP::Request(GET => $url);
 $req->authorization_basic($netid, $password);
-my $response = $ua->request($req);
-
-say $response->as_string();
+my $response = $ua->request($req)->content();
