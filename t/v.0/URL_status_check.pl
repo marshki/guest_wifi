b721e6da450b6cf252c14684099208d0b555d6d9
marshki <marshall@morethantomorrow.org>
2022-01-03 13:41:50 -0500

    works; need to add/test return statement

--- t/v.0/URL_status_check.pl
+++ t/v.0/URL_status_check.pl
@@ -1,16 +1,32 @@
-#!/usr/bin/env bash 
+#!/usr/bin/perl -w
 
-url=""
+use v5.30;
 
-url_check(){ 
-  
-  status_code=$(curl --output /dev/null --silent --head --write-out '%{http_code}\n' $url)
+use strict;
+use warnings;
+use diagnostics;
 
-  if [ $status_code -ne "200" ] ; then 
-    printf "%s\\n" "BAD URL" 
-  else 
-    printf "%s\\n" "GOOD URL" 
-fi 
+our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
+
+my $netid = "";
+my $password = "";
+
+=begin url_check
+Return HTTP response code of web server.
+=cut
+
+sub url_check {
+
+  print "Checking URL status code...\n";
+
+  my $status_code = 
+  (`curl --max-time 2.5 --user ${netid}:${password} --output /dev/null --silent --head --write-out '%{http_code}\n' $url`);
+
+  if ($status_code != '200'){
+    print "URL not accessible. \n";
+  }else{
+    print "URL accessible. \n";
+  }
 }
 
 url_check
