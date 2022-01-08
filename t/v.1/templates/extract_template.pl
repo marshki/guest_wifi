1e35caa5ef04b50c460f8dd112e76e63a908fe55
marshki <marshall@morethantomorrow.org>
2022-01-08 14:04:06 -0500

    these deserve their own dir

---
+++ t/v.1/templates/extract_template.pl
@@ -0,0 +1,28 @@
+#!/usr/bin perl -w
+
+=begin comments
+Template for extracting table from HTML using HTML::TableExtract
+For reference:
+https://metacpan.org/pod/HTML::TableExtract
+=cut
+
+use v5.30;
+
+use strict;
+use warnings;
+use diagnostics;
+
+use HTML::TableExtract;
+
+my $headers = ['Guest ID', 'Password'];
+
+my $table_extract = HTML::TableExtract->new(headers => $headers);
+
+$table_extract->parse_file('sample.html');
+my ($table) = $table_extract->tables;
+
+for my $row ($table->rows) {
+    print 'Guest ID',' ','Password', "\n";
+    print '--------',' ','--------', "\n";
+    print join(" ", @$row), "\n";
+}
