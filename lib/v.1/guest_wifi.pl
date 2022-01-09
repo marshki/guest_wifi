6e075cd9641f23ad2570a0bf387236b7ce9c01a2
marshki <marshall@morethantomorrow.org>
2022-01-09 16:45:04 -0500

    update output

--- lib/v.1/guest_wifi.pl
+++ lib/v.1/guest_wifi.pl
@@ -6,6 +6,60 @@ use strict;
 use warnings;
 use diagnostics;
 
+=pod
+=head1 DESCRIPTION
+
+Extract weekly "nyuguest" WiFi credentials.
+
+=head2 NAME
+
+guest_wifi.pl
+
+=head2 SYNOPSIS
+
+Extract weekly "nyuguest" WiFi credentials using Perl.
+Requires access to NYU-NET--campus wide-area network (WAN)--and a valid NetID.
+Credentials are valid for one(1) week from 12:00:01 EST Monday.
+
+Sample output:
+
+Current time: Sun 09 Jan 2022 04:39:20 PM EST
+Enter your NetID: lilcuckoo0211
+Enter your password: 
+Scraping HTML from NYUROAM page... 
+Parsing region of interest (ROI) from HTML...
+Guest ID Password
+-------- --------
+guest160 giddilot
+
+#### AUTHOR
+
+=head2 AUTHOR
+
+<M. Krinitz> <mjk235 [at] nyu [dot] edu>
+
+=head2 LICENSE
+
+Copyright <2022> <M. Krinitz>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies
+or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
+
+our $date_today = `date`;
+
 use Term::ReadKey;
 use LWP::UserAgent;
 use Mozilla::CA;
@@ -91,6 +145,8 @@ sub print_credentials {
 }
 
 sub main() {
+  print "Current time: $date_today";
+      
   netid_prompt();
   password_prompt();
   scrape_HTML();
