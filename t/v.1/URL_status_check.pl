d92231134772bc9315bc00870ef43290db5c2da8
marshki <marshall@morethantomorrow.org>
2022-02-12 13:25:28 -0500

    testing

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -10,10 +10,8 @@ use LWP::Simple;
 our $url="https://nyuroam-guest.nyu.edu";
 
 =begin URL_status_check
-Check if URL's response header is available.
-Exit if not.
+Exit if URL's response header is not available.
 =cut
-
 
 sub URL_status_check {
 
