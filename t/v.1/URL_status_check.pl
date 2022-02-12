0969a6d9f343bfbe1b15fb32ff8bce443f49e13a
marshki <marshall@morethantomorrow.org>
2022-02-12 13:31:16 -0500

    add TODO comments

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -9,6 +9,11 @@ use LWP::Simple;
 
 our $url="https://nyuroam-guest.nyu.edu";
 
+### TODO: 
+# Set timeout variable
+# Let user know about the wait time
+
+
 =begin URL_status_check
 Exit if URL's response header is not available.
 =cut
