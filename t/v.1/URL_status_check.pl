2c7f9494838199a5b395f6d2f7d42043c8e37aeb
marshki <marshall@morethantomorrow.org>
2022-02-13 12:28:46 -0500

    add

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -7,7 +7,11 @@ use v5.30;
 use warnings;
 use diagnostics;
 
-use LWP::Simple;
+#use LWP::Simple ;
+
+use LWP::Simple qw($ua get) ;
+my $ua = LWP::UserAgent->new;
+$ua->timeout(5);
 
 our $url="https://nyuroam-guest.nyu.edu";
 
@@ -19,6 +23,7 @@ sub URL_status_check {
   
   print "This may take a while...\n";
 
+  $ua->timeout( 1/1000000 );
   if (! head($url)) {
     die "$url NOT reachable!!!\n";
   }
