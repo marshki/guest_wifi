4c83b2275e0016e255c5ad9ada532f8f52b7dad0
marshki <marshall@morethantomorrow.org>
2022-02-13 11:54:55 -0500

    remove note

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# https://metacpan.org/pod/LWP::Simple
+
 use v5.30;
 
 use warnings;
@@ -8,13 +10,6 @@ use diagnostics;
 use LWP::Simple;
 
 our $url="https://nyuroam-guest.nyu.edu";
-
-### TODO: 
-# https://metacpan.org/pod/LWP::Simple
-# Set timeout variable
-# Let user know about the wait time
-
-timeout(7);
 
 =begin URL_status_check
 Exit if URL's response header is not available.
@@ -28,7 +23,3 @@ sub URL_status_check {
 }
 
 URL_status_check;
-
-#if (! head($url)) {
-#  die "$url NOT reachable!!!" 
-#}
