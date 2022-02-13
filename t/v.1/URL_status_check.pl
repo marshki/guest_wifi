bd253265df8f9b0c96b2dbdbe0f325ea6585176f
marshki <marshall@morethantomorrow.org>
2022-02-13 13:32:39 -0500

    grr

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -1,32 +1,17 @@
 #!/usr/bin/perl
 
-# https://metacpan.org/pod/LWP::Simple
-
 use v5.30;
 
 use warnings;
 use diagnostics;
 
-#use LWP::Simple ;
-
 use LWP::Simple qw($ua get) ;
-my $ua = LWP::UserAgent->new;
 $ua->timeout(5);
 
 our $url="https://nyuroam-guest.nyu.edu";
 
-=begin URL_status_check
-Exit if URL's response header is not available.
-=cut
+#=begin URL_status_check
+#Exit if URL's is not retrievable.
+#=cut
 
-sub URL_status_check {
-  
-  print "This may take a while...\n";
-
-  $ua->timeout( 1/1000000 );
-  if (! head($url)) {
-    die "$url NOT reachable!!!\n";
-  }
-}
-
-URL_status_check;
+my $html = get $url || die "Request timed out!\n" ;
