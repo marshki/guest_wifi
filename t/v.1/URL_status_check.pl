937a4fb992ce8aeb5b760568a484334ea678b8b3
marshki <marshall@morethantomorrow.org>
2022-02-19 10:34:15 -0500

    update

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -5,13 +5,14 @@ use v5.30;
 use warnings;
 use diagnostics;
 
-use LWP::Simple qw($ua get) ;
+use LWP::Simple qw($ua get);
 $ua->timeout(5);
 
 our $url="https://nyuroam-guest.nyu.edu";
 
-#=begin URL_status_check
-#Exit if URL's is not retrievable.
-#=cut
+=begin URL_status_check
+Die if URL is not retrievable.
+=cut
 
-my $html = get $url || die "Request timed out!\n" ;
+my $html = get $url || die "Request timed out!\n";
+
