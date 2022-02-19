e3ac99bbfe5d9a37f42d9b31d22a9f1f576f1df3
marshki <marshall@morethantomorrow.org>
2022-02-19 10:30:14 -0500

    rm excess file

--- t/v.1/test.pl
+++
@@ -1,9 +0,0 @@
-# Note that if you do this, you must explicitly
-# export everything you want to use:
-use LWP::Simple qw($ua get);
-
-our $url="https://nyuroam-guest.nyu.edu";
-
-$ua->timeout(5);	
-
-my $html = get $url || die "Request timed out!\n" ;
