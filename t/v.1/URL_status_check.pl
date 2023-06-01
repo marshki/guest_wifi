03c885a0a63d68371ff49a5b80228ca0ba871e3e
marshki <marshall@morethantomorrow.org>
2023-06-01 10:29:05 -0400

    comments

--- t/v.1/URL_status_check.pl
+++ t/v.1/URL_status_check.pl
@@ -4,7 +4,6 @@ use v5.30;
 
 use strict;
 use warnings;
-use diagnostics;
 
 use LWP::Simple qw($ua get);
 $ua->timeout(5);
@@ -12,8 +11,8 @@ $ua->timeout(5);
 our $url="https://nyuroam-guest.nyu.edu";
 
 =begin URL_status_check
-Max time to wait for server response is five (5) seconds.
 Die (quit) if URL is not reachable.
+Max wait for resply is five (5) seconds.
 =cut
 
 sub URL_status_check {
