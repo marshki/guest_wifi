23cca60b4f6c19edd765f976ce0c99ad543fdf6d
marshki <marshall@morethantomorrow.org>
2023-06-02 11:12:03 -0400

    fix failed test

--- t/v.1/scrape_HTML.pl
+++ t/v.1/scrape_HTML.pl
@@ -11,22 +11,32 @@ use Mozilla::CA;
 
 our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
-my $netid = "";
-my $password = "";
+my $netid = '';
+my $password = '';
 
 my $ua = LWP::UserAgent->new();
+$ua->timeout(5);  # Set timeout value in seconds
 my $req = HTTP::Request->new(GET => $url);
 
 our $HTML;
 
 sub scrape_HTML {
-
     print "Scraping HTML from NYUROAM page... \n";
-
     $req->authorization_basic($netid, $password);
-
-    return $HTML = $ua->request($req)->content();
+    my $response = $ua->request($req);
+    if ($response->is_success) {
+        return $HTML = $response->content;
+    } else {
+        die "Failed to scrape HTML: " . $response->status_line;
+    }
 }
 
-my $actual_html = scrape_HTML();
-ok($actual_html, "Scraped HTML is non-empty.");
+# Test scrape_HTML
+eval {
+    my $actual_html = scrape_HTML();
+    ok(length($actual_html) > 0, "Scraped HTML is non-empty");
+};
+
+if ($@) {
+    ok(0, "Test failed: $@");
+}
