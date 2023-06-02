3a493260cab0c20c2bc17d18ace16ac6ed4fca5e
marshki <marshall@morethantomorrow.org>
2023-06-02 10:44:15 -0400

    test for empty HTML in event of failure

--- t/v.1/scrape_HTML.pl
+++ t/v.1/scrape_HTML.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
 
 use v5.30;
-
 use strict;
 use warnings;
 
@@ -14,35 +13,20 @@ our $url = "https://nyuroam-guest.nyu.edu/cgi-bin/index.pl";
 
 my $netid = "";
 my $password = "";
-my @HTML;
-     
+
 my $ua = LWP::UserAgent->new();
-my $req = new HTTP::Request(GET => $url);
+my $req = HTTP::Request->new(GET => $url);
 
 our $HTML;
 
-=begin comment
-Scrape, then return parsed table to "HTML".
-=end comment
-=cut
-
 sub scrape_HTML {
-  print "Scraping HTML from NYUROAM page... \n";
 
-  $req->authorization_basic($netid, $password);
+    print "Scraping HTML from NYUROAM page... \n";
 
-  return $HTML = $ua->request($req)->content();
+    $req->authorization_basic($netid, $password);
+
+    return $HTML = $ua->request($req)->content();
 }
 
-ok( @HTML eq @HTML );
-
-=begin comment
-# ... Code omitted ...
-
-# Test the scrape_HTML subroutine
-subtest "Test scrape_HTML" => sub {
-    my $actual_html = scrape_HTML();
-    ok($actual_html, "Scraped HTML is non-empty");
-};
-=end comment
-=cut
+my $actual_html = scrape_HTML();
+ok($actual_html, "Scraped HTML is non-empty.");
